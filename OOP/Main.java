698607e382fe04b15871c974009674f345e8251c
evananyonga <evewish@gmail.com>
2024-02-25 15:21:51 +0300

    Modify object attribute

--- OOP/Main.java
+++ OOP/Main.java
@@ -1,8 +1,13 @@
 package OOP;
 
 public class Main {
+        String greeting = "Hello";
     public static void main(String[] args) {
+        Main food = new Main();
+        food.greeting = "Hello Eva";
+
         Oops myImport = new Oops();
         System.out.println(myImport.x);
+        System.out.println(food.greeting);
     }
 }

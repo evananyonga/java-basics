582454872841bf7e9a929352855506256ca523cd
evananyonga <evewish@gmail.com>
2024-02-27 13:13:18 +0300

    achieving encapsulation

---
+++ OOP/Encapsulation.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class Encapsulation {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String someName) {
+        this.name = someName;
+    }
+}
--- OOP/Main.java
+++ OOP/Main.java
@@ -3,21 +3,35 @@ package OOP;
 public class Main {
         String greeting = "Hello";
     public static void main(String[] args) {
+        // Accessing and modifying class attributes
         Main food = new Main();
         food.greeting = "Hello Eva";
 
-        Oops myImport = new Oops();
-        System.out.println(myImport.x);
         System.out.println(food.greeting);
 
+        // Importing attributes from other classes
+        Oops myImport = new Oops();
+        System.out.println(myImport.x);
+
+        // Importing multiple class methods
         Oops myCar = new Oops();
         myCar.fullThrottle();
         myCar.speed(200);
 
+        // Using access/non-access modifiers
         Student stud = new Student();
 
         System.out.println("Name: " + stud.first_name);
         System.out.println("Age: " + stud.age);
         System.out.println("Name: " + stud.grad_year);
+
+        // Encapsulation
+        Encapsulation lad = new Encapsulation();
+        // lad.name = "Yokaana"; throws an error
+        // System.out.println(lad.name); also throws an error
+
+        // Proper way to use encapsulated class attributes
+        lad.setName("Yokie");
+        System.out.println(lad.getName()); 
     }
 }
